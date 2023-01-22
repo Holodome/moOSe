d4ebf94dd1577119207db70dae2b3e2aff5c32ae
Holodome <ilal.vinogradov@gmail.com>
2023-01-22 23:46:30 +0300

    finally boot kernel

--- Makefile
+++ Makefile
@@ -11,6 +11,11 @@ export CC LD AS OBJCOPY
 ASFLAGS := -msyntax=att --warn --fatal-warnings
 CFLAGS  := -Wall -Werror -Wextra -Wpedantic -std=c99 -ffreestanding -nostdlib -nostartfiles -Wl,-r
 
+ifneq ($(DEBUG),)
+	ASFLAGS += -ggdb
+	CFLAGS += -ggdb
+endif
+
 export ASFLAGS CFLAGS
 
 TARGET_IMG := moose.img
@@ -24,6 +29,10 @@ $(TARGET_IMG): $(OS_IMG)
 
 qemu: all
 	qemu-system-i386 -fda moose.img
+
+qemu-debug: all
+	qemu-system-i386 -s -S -fda moose.img -d guest_errors,int &
+  	i386-elf-gdb -ex "target remote localhost:1234" -ex "symbol-file moose/kernel/kernel.elf"
 
 clean:
 	rm $(shell find . -name "*.o" -o -name "*.d" -o -name "*.bin" -o -name "*.img")
--- moose/kernel/arch/boot/boot.s
+++ moose/kernel/arch/boot/boot.s
@@ -1,33 +1,23 @@
 .code16
 
-// Stack pointer value
-.equ STACK, 0x8000
-// Where to load kernel
-.equ KERNEL_OFFSET, 0x1000
-
 boot:
-    // Set up registers because bios does not guarantee their contents
-    xorw %ax, %ax
-    movw %ax, %ds
-    movw %ax, %es
-    movw STACK, %bx
-    movb %dl, boot_drive
+    mov $0x9000, %bp
+    mov %bp, %sp
 
-    cli
-    movw %bx, %ss
-    movw %ax, %sp
-    sti
-    cld
+    mov $0x1000, %bx
+    mov $0x2, %dh
+    mov $0x00, %dl
 
-    push intro_msg_len
-    push $intro_msg
-    call print_msg
+    pusha
+    push %dx
+    mov $0x02, %ah
+    mov %dh, %al      // sectors to read
+    mov $0x02, %cl      // sector number
+    mov $0x00, %ch      // cylinder
+    mov $0x00, %dh      // head
 
-    // load kernel
-    mov KERNEL_OFFSET, %bx
-    movb $2, %dh
-    movb boot_drive, %dl
-    call disk_load
+    int $0x13
+    jc halt
 
     jmp switch_to_32bit
 halt:
@@ -35,58 +25,41 @@ halt:
 
 // Prints message with str, len passed on stack
 print_msg:
-    movw %sp, %bp
+    pusha
 
     # Get cursor position
-    movb $0x03, %ah
+    mov $0x03, %ah
     # Set the color for next interrupt and page 0
-    movw $0x0007, %bx
+    mov $0x0007, %bx
     int $0x10
     # Write string, move cursor
-    movw $0x1301, %ax
-    movw 4(%bp), %cx
-    movw 2(%bp), %bp
+    mov $0x1301, %ax
+    mov disk_error_msg_len, %cx
+    mov $disk_error_msg, %bp
     int $0x10
-
-    ret
-
-// Reads dh number of sectors from disk to es:bx
-disk_load:
-    pusha
-    push %dx
-
-    movb $0x02, %ah
-    movb %dh, %al
-    movw $0x0002, %cx
-    movb $0x00, %dh
-
-    int $0x13
-    jc disk_error
-
-    pop %dx
-    cmp %al, %dh
-    jne disk_error
 
     popa
     ret
+
 disk_error:
-    push disk_error_msg_len
-    push $disk_error_msg
-    call print_msg
+    //push disk_error_msg_len
+    //push $disk_error_msg
+    //call print_msg
     jmp halt
 
 // GDT that resembles flat memory model
 
 // null segment
-gdt_start: .quad 0x0
+gdt_start:
+    .quad 0x0
 
 // code segment
 gdt_code:
     .word 0xffff // segment length
     .word 0x0000 // segment base
     .byte 0x00   // segment base upper bits
-    .byte 0x9A   // flags
-    .byte 0xCF   // flags + segment length
+    .byte 0b10011010   // flags
+    .byte 0b11001111   // flags + segment length
     .byte 0x00   // segment base upper bits
 
 // data segment
@@ -94,8 +67,8 @@ gdt_data:
     .word 0xffff // segment length
     .word 0x0000 // segment base
     .byte 0x00   // segment base upper bits
-    .byte 0x9A   // flags
-    .byte 0xCF   // flags + segment length
+    .byte 0b10010010   // flags
+    .byte 0b11001111   // flags + segment length
     .byte 0x00   // segment base upper bits
 
 gdt_end:
@@ -114,21 +87,21 @@ switch_to_32bit:
     movl %cr0, %eax
     orl $0x1, %eax // enable protected mode
     movl %eax, %cr0
-    jmp $CODE_SEG, $init_32bit
+    ljmp $0x0008, $init_32bit
 
 .code32
 init_32bit:
-    movw DATA_SEG, %ax
-    movw %ax, %ds
-    movw %ax, %ss
-    movw %ax, %es
-    movw %ax, %fs
-    movw %ax, %gs
+    mov $0x0010, %ax
+    mov %ax, %ds
+    mov %ax, %ss
+    mov %ax, %es
+    mov %ax, %fs
+    mov %ax, %gs
 
-    movl 0x90000, %ebp
-    movl %esp, %ebp
+    movl $0x90000, %ebp
+    movl %ebp, %esp
 
-    jmp KERNEL_OFFSET
+    ljmp $0x0008,$0x1000
 
 boot_drive: .byte 0
 intro_msg: .ascii "running moOSe stage 1 bootloader\r\n"
