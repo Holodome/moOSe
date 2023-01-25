8fce82d09a431731c117387246761840d624a7c9
Holodome <ilal.vinogradov@gmail.com>
2023-01-25 15:20:58 +0400

    add bss to kernel image

--- moose/arch/entry.s
+++ moose/arch/entry.s
@@ -1,4 +1,13 @@
 .extern _kmain
 start:
+    // zero the bss
+    cld
+    mov $__bss_start, %edi
+    mov $__bss_end, %ecx
+    sub %ecx, %edi
+    xor %eax, %eax
+    shr $2, %ecx
+    rep; stosl
+
     call _kmain
     jmp .
