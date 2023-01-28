c0098078f6ae6cbf99b2874d37d6224c49abc5a7
Holodome <ilal.vinogradov@gmail.com>
2023-01-28 21:08:21 +0400

    arch/amd64: zero bss and jump to kmain

---
+++ moose/kmain.c
@@ -0,0 +1,8 @@
+#include "kstdio.h"
+
+__attribute__((noreturn)) void kmain(void) {
+    kputs("running moOSe kernel");
+    kprintf("build %s %s\n", __DATE__, __TIME__);
+    for (;;)
+        ;
+}
