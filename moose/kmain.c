1b726c5b8e919ed197be9121fc9967cc56b6494a
Holodome <ilal.vinogradov@gmail.com>
2023-01-31 22:41:33 +0300

    kernel: move fs test from main

--- moose/kmain.c
+++ moose/kmain.c
@@ -15,7 +15,7 @@ struct fatfs_cursor {
     u32 start;
 };
 
-ssize_t read(void *handle, void *buf, size_t size) {
+static ssize_t read(void *handle, void *buf, size_t size) {
     struct fatfs_cursor *cursor = handle;
 
     u32 lba = cursor->pos / 512 + cursor->start;
@@ -33,14 +33,14 @@ ssize_t read(void *handle, void *buf, size_t size) {
     return size;
 }
 
-ssize_t write(void *handle, const void *buf, size_t size) {
+static ssize_t write(void *handle, const void *buf, size_t size) {
     (void)handle;
     (void)buf;
     (void)size;
     return -1;
 }
 
-ssize_t seek(void *handle, i32 off, int whence) {
+static ssize_t seek(void *handle, i32 off, int whence) {
     struct fatfs_cursor *cursor = handle;
     switch (whence) {
     case PFATFS_SEEK_CUR:
@@ -58,22 +58,7 @@ ssize_t seek(void *handle, i32 off, int whence) {
     return 0;
 }
 
-__attribute__((noreturn)) void kmain(void) {
-    kputs("running moOSe kernel");
-    kprintf("build %s %s\n", __DATE__, __TIME__);
-
-    const struct memmap_entry *memmap;
-    u32 memmap_size;
-    get_memmap(&memmap, &memmap_size);
-    kprintf("Bios-provided physical RAM map:\n");
-    for (u32 i = 0; i < memmap_size; ++i) {
-        const struct memmap_entry *entry = memmap + i;
-        kprintf("%#016llx-%#016llx: %s(%u)\n", (unsigned long long)entry->base,
-                (unsigned long long)(entry->base + entry->length),
-                get_memmap_type_str(entry->type), (unsigned)entry->type);
-    }
-
-    setup_idt();
+__attribute__((unused)) static void test_fs(void) {
     struct pfatfs_settings settings = {
         .read = read, .write = write, .seek = seek};
     struct pfatfs fs = {.settings = &settings};
@@ -109,6 +94,24 @@ __attribute__((noreturn)) void kmain(void) {
         }
         kprintf("%u: %11s: %u\n", i, (char *)new_file.name, new_file.size);
     }
+}
+
+__attribute__((noreturn)) void kmain(void) {
+    kputs("running moOSe kernel");
+    kprintf("build %s %s\n", __DATE__, __TIME__);
+
+    const struct memmap_entry *memmap;
+    u32 memmap_size;
+    get_memmap(&memmap, &memmap_size);
+    kprintf("Bios-provided physical RAM map:\n");
+    for (u32 i = 0; i < memmap_size; ++i) {
+        const struct memmap_entry *entry = memmap + i;
+        kprintf("%#016llx-%#016llx: %s(%u)\n", (unsigned long long)entry->base,
+                (unsigned long long)(entry->base + entry->length),
+                get_memmap_type_str(entry->type), (unsigned)entry->type);
+    }
+
+    setup_idt();
 
     for (;;)
         ;
