0dd9df2d232fe315d32b233048c67f1fbe32d36a
Holodome <ilal.vinogradov@gmail.com>
2023-01-31 23:38:26 +0300

    arch/amd64: add ptrdiff_t, uintmax_t, intmax_t

--- moose/include/arch/amd64/types.h
+++ moose/include/arch/amd64/types.h
@@ -12,6 +12,9 @@ typedef unsigned long long int u64;
 typedef unsigned long long int size_t;
 typedef unsigned long long int uintptr_t;
 typedef signed long long int ssize_t;
+typedef signed long long int ptrdiff_t;
+typedef signed long long int intmax_t;
+typedef unsigned long long int uintmax_t;
 
 #define CHAR_BIT 8
 #define NULL ((void *)0)
