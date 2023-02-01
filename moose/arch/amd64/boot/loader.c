9e1c3ea39fcca3c52e37d9b27459ef453b2ab97d
Holodome <ilal.vinogradov@gmail.com>
2023-02-01 18:57:17 +0300

    arch/amd64: start trying to read kernel from fat disk

--- moose/arch/amd64/boot/loader.c
+++ moose/arch/amd64/boot/loader.c
@@ -2,7 +2,55 @@
 #include <../disk.c>
 #include <../fs/fat.c>
 
+struct fatfs_cursor {
+    struct pfatfs *fs;
+    u32 pos;
+
+    u8 block[512];
+    u32 current_block;
+    u32 start;
+};
+
+static ssize_t read(void *handle __attribute__((unused)), void *buf,
+                    size_t size) {
+    return disk_read(buf, size);
+}
+
+static ssize_t write(void *handle __attribute__((unused)), const void *buf,
+                     size_t size) {
+    (void)handle;
+    (void)buf;
+    (void)size;
+    return -1;
+}
+
+static ssize_t seek(void *handle __attribute__((unused)), off_t off,
+                    int whence) {
+    if (whence == SEEK_SET) {
+        off += 9216 + 512;
+    }
+    return disk_seek(off, whence);
+}
+
+extern void print(const char *s);
+
 int load_kernel(void) {
-    /* return 0; */
-    return ata_pio_read((void *)0x100000, 18, 1024);
+    struct pfatfs fs = {.settings = &(struct pfatfs_settings){
+                            .seek = seek, .read = read, .write = write}};
+    int result = pfatfs_mount(&fs);
+    if (result != 0) {
+        print("failed to mount");
+        return result;
+    }
+
+    struct pfatfs_file file;
+    result = pfatfs_open(&fs, "kernel1.bin", &file);
+    if (result != 0) {
+        print("failed to open");
+        return result;
+    }
+
+    for (;;);
+
+    return 0;
 }
