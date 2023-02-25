89119fce1007a25ee4b476a8058da65dbaa77052
Holodome <ilal.vinogradov@gmail.com>
2023-02-26 00:17:19 +0300

    arch/amd64: use irq_save in virmem.c

--- moose/include/arch/cpu.h
+++ moose/include/arch/cpu.h
@@ -1,15 +1,10 @@
 #pragma once
 
 #include <types.h>
+#include <arch/amd64/cpu.h>
 
 void dump_registers(void);
 __attribute__((noreturn)) void halt_cpu(void);
 
 void set_stack(u64 sp, u64 old_stack_base);
-
-void irq_disable(void);
-void irq_enable(void);
-int irqs_disabled(void);
-void irq_save(volatile unsigned long *dst);
-void irq_restore(unsigned long src);
 
