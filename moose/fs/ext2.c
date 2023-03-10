09788dc2bed98f3aef1366435a02066a7e53d07f
Holodome <ilal.vinogradov@gmail.com>
2023-03-10 15:12:49 +0300

    fs/ext2: add freeing of blocks and inodes

--- moose/fs/ext2.c
+++ moose/fs/ext2.c
@@ -10,30 +10,39 @@
 #define EXT2_SB_OFFSET 1024
 #define EXT2_BGD_OFFSET 2048
 
-static u32 calculate_inode_phys_offset(struct ext2_fs *fs, u32 ino) {
+static void calc_block_group(const struct ext2_fs *fs, u32 block, u32 *group,
+                             u32 *in_group) {
+    *group = block / fs->sb.s_blocks_per_group;
+    *in_group = block % fs->sb.s_blocks_per_group;
+}
+
+static void calc_ino_group(const struct ext2_fs *fs, u32 ino, u32 *group,
+                           u32 *in_group) {
     assert(ino);
     --ino;
-    u32 ino_group = ino / fs->sb.s_inodes_per_group;
-    u32 ino_in_group = ino % fs->sb.s_inodes_per_group;
-    u32 ino_block_in_group = ino_in_group / fs->inodes_per_block;
-    u32 ino_in_block = ino_in_group % fs->inodes_per_block;
-    assert(ino_group < fs->bgds_count);
-    u32 ino_block = fs->bgds[ino_group].bg_inode_table + ino_block_in_group;
-    u32 ino_offset = ino_in_block * sizeof(struct ext2_inode);
+    *group = ino / fs->sb.s_inodes_per_group;
+    *in_group = ino % fs->sb.s_inodes_per_group;
+}
 
-    return ino_block * fs->block_size + ino_offset;
+static u32 calc_inode_phys_offset(const struct ext2_fs *fs, u32 ino) {
+    u32 ino_group, ino_in_group;
+    calc_ino_group(fs, ino, &ino_group, &ino_in_group);
+    assert(ino_group < fs->bgds_count);
+    u32 ino_table = fs->bgds[ino_group].bg_inode_table;
+    u32 ino_offset = ino_in_group * sizeof(struct ext2_inode);
+    return ino_table * fs->block_size + ino_offset;
 }
 
 static int read_inode(struct ext2_fs *fs, struct ext2_inode *inode, u32 ino) {
     assert(ino);
-    u32 inode_offset = calculate_inode_phys_offset(fs, ino);
+    u32 inode_offset = calc_inode_phys_offset(fs, ino);
     return seek_read(fs->dev, inode_offset, inode, sizeof(*inode));
 }
 
 static int write_inode(struct ext2_fs *fs, const struct ext2_inode *inode,
                        u32 ino) {
     assert(ino);
-    u32 inode_offset = calculate_inode_phys_offset(fs, ino);
+    u32 inode_offset = calc_inode_phys_offset(fs, ino);
     return seek_write(fs->dev, inode_offset, inode, sizeof(*inode));
 }
 
@@ -64,7 +73,7 @@ static ssize_t alloc_ino(struct ext2_fs *fs, int is_dir) {
         return -EIO;
 
     u64 found = bitmap_first_clear(bitmap, fs->sb.s_inodes_per_group);
-    if (!found) panic("Corrupted filesystem");
+    assert(found);
 
     assert(desc->bg_free_inodes_count);
     --desc->bg_free_inodes_count;
@@ -77,11 +86,36 @@ static ssize_t alloc_ino(struct ext2_fs *fs, int is_dir) {
                    sizeof(bitmap)) < 0)
         return -EIO;
 
-    if (sync_superblock(fs) < 0)
-        return -EIO;
+    if (sync_superblock(fs) < 0) return -EIO;
 
     u32 ino = bgdi * fs->sb.s_inodes_per_group + found;
     return ino;
+}
+
+static int free_ino(struct ext2_fs *fs, u32 ino, int is_dir) {
+    u32 ino_group, ino_in_group;
+    calc_ino_group(fs, ino, &ino_group, &ino_in_group);
+    assert(ino_group < fs->bgds_count);
+    struct ext2_group_desc *group = fs->bgds + ino_group;
+    u64 bitmap[fs->group_inode_bitmap_size];
+    if (seek_read(fs->dev, group->bg_inode_bitmap * fs->block_size, bitmap,
+                  sizeof(bitmap)) < 0)
+        return -EIO;
+
+    assert(test_bit(ino_in_group, bitmap));
+
+    clear_bit(ino_in_group, bitmap);
+    ++group->bg_free_inodes_count;
+    assert(!is_dir || group->bg_used_dirs_count);
+    group->bg_used_dirs_count -= is_dir;
+    ++fs->sb.s_free_inode_count;
+
+    if (seek_write(fs->dev, group->bg_inode_bitmap * fs->block_size, bitmap,
+                   sizeof(bitmap)) < 0)
+        return -EIO;
+
+    if (sync_superblock(fs) < 0) return -EIO;
+    return 0;
 }
 
 static ssize_t alloc_block(struct ext2_fs *fs) {
@@ -114,10 +148,34 @@ static ssize_t alloc_block(struct ext2_fs *fs) {
                    sizeof(bitmap)) < 0)
         return -EIO;
 
-    if (sync_superblock(fs) < 0)
-        return -EIO;
+    if (sync_superblock(fs) < 0) return -EIO;
 
     return bgdi * fs->sb.s_blocks_per_group + found - 1;
+}
+
+static int free_block(struct ext2_fs *fs, u32 block) {
+    u32 block_group, block_in_group;
+    calc_block_group(fs, block, &block_group, &block_in_group);
+    assert(block_group < fs->bgds_count);
+    struct ext2_group_desc *group = fs->bgds + block_group;
+    u64 bitmap[fs->group_block_bitmap_size];
+    if (seek_read(fs->dev, group->bg_block_bitmap * fs->block_size, bitmap,
+                  sizeof(bitmap)) < 0)
+        return -EIO;
+
+    assert(test_bit(block_in_group, bitmap));
+
+    clear_bit(block_in_group, bitmap);
+    ++group->bg_free_blocks_count;
+    ++fs->sb.s_free_block_count;
+
+    if (seek_write(fs->dev, group->bg_block_bitmap * fs->block_size, bitmap,
+                   sizeof(bitmap)) < 0)
+        return -EIO;
+
+    if (sync_superblock(fs) < 0) return -EIO;
+
+    return 0;
 }
 
 int ext2_mount(struct ext2_fs *fs) {
