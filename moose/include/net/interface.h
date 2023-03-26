86ce8d14a4742c53eb34507fdd0147299b18f27d
paw1a <paw1a@yandex.ru>
2023-03-26 12:41:55 +0300

    kernel: add network interface and device

---
+++ moose/include/net/interface.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <net/device.h>
+
+#define IFNAME_SIZE 16
+
+struct net_interface {
+    u8 mac_addr[6];
+    u8 ip_addr[4];
+    u8 broadcast_ip_addr[4];
+    u8 net_mask[4];
+    char name[IFNAME_SIZE];
+    struct net_device *dev;
+    struct list_head list;
+};
+
+struct net_interface *create_net_interface(const char *name,
+                                           struct net_device *dev);
+void remove_net_interface(struct net_interface *net_if);
---
+++ moose/net/interface.c
@@ -0,0 +1,29 @@
+#include <net/interface.h>
+#include <mm/kmalloc.h>
+#include <string.h>
+#include <list.h>
+
+LIST_HEAD(interfaces);
+
+struct net_interface *create_net_interface(const char *name,
+                                           struct net_device *dev) {
+    struct net_interface *net_if = kzalloc(sizeof(*net_if));
+    if (net_if == NULL)
+        return NULL;
+
+    size_t name_size = strlen(name);
+    if (name_size > IFNAME_SIZE)
+        return NULL;
+
+    memcpy(net_if->name, name, name_size);
+    net_if->dev = dev;
+
+    list_add(&net_if->list, &interfaces);
+
+    return net_if;
+}
+
+void remove_net_interface(struct net_interface *net_if) {
+    list_remove(&net_if->list);
+    kfree(net_if);
+}
