080d1850c05e110b0718a0946fd557b25222880e
Holodome <ilal.vinogradov@gmail.com>
2023-05-31 21:50:06 +0300

    libc: implement ctype.h

---
+++ userspace/libc/ctype.h
@@ -0,0 +1,18 @@
+#pragma once 
+
+int isdigit(int c);
+int isalnum(int c);
+int isalpha(int c);
+int iscntrl(int c);
+int isgraph(int c);
+int islower(int c);
+int isupper(int c);
+int isxdigit(int c);
+int isspace(int c);
+int ispunct(int c);
+int isascii(int c);
+int isodigit(int c);
+
+int tolower(int c);
+int toupper(int c);
+int toascii(int c);
